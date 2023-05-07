838180917954e98ffeaea9853466a030a15aab27
yugojyoti <yugojyotimohanta@gmail.com>
2023-05-07 07:19:56 +0530

    asset and theme done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:mini_store/consts/global_colors.dart';
+
+import 'screens/home_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,6 +19,32 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: "Mini Store",
+      theme: ThemeData(
+          scaffoldBackgroundColor: lightScaffoldColor,
+          primaryColor: lightCardColor,
+          backgroundColor: lightBackground,
+          appBarTheme: AppBarTheme(
+            iconTheme: IconThemeData(color: lightIconsColor),
+            backgroundColor: lightScaffoldColor,
+            centerTitle: true,
+            titleTextStyle: TextStyle(
+              color: lightTextColor,
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
+            elevation: 0,
+          ),
+          iconTheme: IconThemeData(
+            color: lightTextColor,
+          ),
+          textSelectionTheme: TextSelectionThemeData(
+              cursorColor: Colors.black, selectionColor: Colors.blue),
+          cardColor: lightCardColor,
+          brightness: Brightness.light,
+          colorScheme: ThemeData().colorScheme.copyWith(
+              secondary: lightIconsColor, brightness: Brightness.light)),
+      home: HomeScreen(),
     );
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/widgets.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        body: Center(
+          child: Text('Welcome TO the Course'),
+        ),
+      ),
+    );
+  }
+}
